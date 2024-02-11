2b3d387d4d26d6a1907dff1275e54813c174001a
Luiz Guilherme <contato.guilhermealmeida@gmail.com>
2024-02-11 04:54:55 -0300

    add episodes list

--- script/api.js
+++ script/api.js
@@ -1,9 +1,9 @@
 let personagem = Math.floor(Math.random() * 826) + 1;
-const url = `https://rickandmortyapi.com/api/character/${personagem}`
-async function getApi(){
-    const response = await fetch(url)
-    if (response.status == 200){
-        const dados = await response.json()
-        insetDataHtml(dados)
-    }
+const url = `https://rickandmortyapi.com/api/character/${personagem}`;
+async function getApi() {
+  const response = await fetch(url);
+  if (response.status == 200) {
+    const dados = await response.json();
+    insetDataHtml(dados);
+  }
 }
--- script/script.js
+++ script/script.js
@@ -1,30 +1,47 @@
 const data = {
-    name: document.getElementById('name'),
-    status: document.getElementById("status"),
-    species: document.getElementById("species"),
-    type: document.getElementById("type"),
-    gender: document.getElementById("gender"),
-    picture: document.querySelector(".picture")
+  name: document.getElementById("name"),
+  status: document.getElementById("status"),
+  species: document.getElementById("species"),
+  type: document.getElementById("type"),
+  gender: document.getElementById("gender"),
+  picture: document.querySelector(".picture"),
+  epsodes: document.querySelector(".episodes"),
+};
+
+function lifeStatus(apidata) {
+  if (apidata.status == "Alive") {
+    data.status.classList.add("alive");
+  } else if (apidata.status == "Dead") {
+    data.status.classList.add("dead");
+  } else {
+    data.status.classList.add("unknown");
+  }
 }
 
-function lifeStatus(apidata){
-    if(apidata.status == 'Alive'){
-        data.status.classList.add("alive")
-    }else if(apidata.status == 'Dead'){
-        data.status.classList.add("dead")
-    }else{
-        data.status.classList.add("unknown")
+async function displayed(apidata) {
+  try {
+    for (const element of apidata.episode) {
+      const parts = element.split("/");
+      const number = parts[5];
+      const line = document.createElement("li");
+      line.innerText = `Episode: ${number}`;
+      await data.epsodes.appendChild(line);
     }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-function insetDataHtml(apidata){
-    data.name.innerText = apidata.name
-    data.status.innerText = apidata.status
-    data.species.innerText = apidata.species
-    data.type.innerText = apidata.type
-    data.gender.innerText = apidata.gender
-    data.picture.src = apidata.image
-    lifeStatus(apidata)
+function insetDataHtml(apidata) {
+  data.name.innerText = apidata.name;
+  data.status.innerText = apidata.status;
+  data.species.innerText = apidata.species;
+  data.type.innerText = apidata.type;
+  data.gender.innerText = apidata.gender;
+  data.picture.src = apidata.image;
+  lifeStatus(apidata);
+
+  displayed(apidata);
 }
 
-getApi()
+getApi();
